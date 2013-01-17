7a985d12f2fccda3562f61dc6a9a69cce8d92bbc
sciascid <sciascid@4b45d5e3-6afd-4c2a-bf5d-36b89e0d2062>
2013-01-17 14:34:24 +0000

    Refactor learner_state out of learner. Makes learner much more flexible, and easier to test...
    git-svn-id: svn+ssh://atelier.inf.usi.ch/home/sciascid/svnroot/libpaxos@609 4b45d5e3-6afd-4c2a-bf5d-36b89e0d2062

--- lib/learner.c
+++ lib/learner.c
@@ -9,34 +9,13 @@
 
 #include "libpaxos.h"
 #include "libpaxos_priv.h"
+#include "learner_state.h"
 #include "paxos_net.h"
 #include "config_reader.h"
 
-#define INST_INFO_EMPTY (0)
-#define IS_CLOSED(INST) (INST->final_value != NULL)
-
-//Structure used to store all info relative to a given instance
-struct instance
-{
-    iid_t           iid;
-    ballot_t        last_update_ballot;
-    accept_ack*     acks[N_OF_ACCEPTORS];
-    accept_ack*     final_value;
-};
-
 struct learner
 {
-	//Highest instance for which a message was seen
-	iid_t highest_iid_seen; 
-	// Current instance, incremented when current is closed and 
-	// the corresponding value is delivered
-	iid_t current_iid;
-	// Highest instance that is already closed 
-	// (can be higher than current!)
-	// TODO: not used
-	iid_t highest_iid_closed; 
-	// Array (used as a circular buffer) to store instance infos
-	struct instance instances[LEARNER_ARRAY_SIZE];
+	struct learner_state* state;
 	// Function to invoke when the current_iid is closed, 
 	// the final value and some other informations is passed as argument
 	deliver_function delfun;
@@ -50,280 +29,28 @@ struct learner
 	struct bufferevent* acceptor_ev[N_OF_ACCEPTORS];
 };
 
-/*-------------------------------------------------------------------------*/
-// Helpers
-/*-------------------------------------------------------------------------*/
-
-static struct instance*
-learner_get_instance(struct learner* l, iid_t iid)
-{
-	// This is equivalent to n mod LEARNER_ARRAY_SIZE, 
-	// works only if LEARNER_ARRAY_SIZE is a power of 2.
-	
-	return &(l->instances[iid & (LEARNER_ARRAY_SIZE-1)]);
-}
-
-// Reset all fields and free stored messages
-// of a learner instance
-static void
-learner_clear_instance(struct learner* l, iid_t iid)
-{
-	int i;
-	struct instance* ii;
-	
-	ii = learner_get_instance(l, iid);
-    ii->iid = INST_INFO_EMPTY;
-    ii->last_update_ballot = 0;
-    ii->final_value = NULL;
-
-    // Free all stored accept_ack
-    for (i = 0; i < N_OF_ACCEPTORS; i++) {
-        if (ii->acks[i] != NULL) {
-            PAX_FREE(ii->acks[i]);
-            ii->acks[i] = NULL;            
-        }
-    }
-}
-
-// Stores the accept_ack of a given acceptor in the corresponding record 
-// at the appropriate index
-// Assumes rec->acks[acc_id] is NULL already
 static void 
-instance_store_accept(struct instance* ii, accept_ack * aa)
-{
-	accept_ack * new_ack;
-	
-	new_ack = PAX_MALLOC(ACCEPT_ACK_SIZE(aa));
-    memcpy(new_ack, aa, ACCEPT_ACK_SIZE(aa));
-    //Store message at appropriate index (acceptor_id)
-    ii->acks[aa->acceptor_id] = new_ack;
-    //Keep track of most recent accept_ack stored
-    ii->last_update_ballot = aa->ballot;
-}
-
-// Tries to update the state based on the accept_ack received.
-// Returns 0 if the message was discarded because not relevant. 1 if the state changed.
-static int
-instance_update(struct instance* ii, accept_ack * aa)
-{
-    // First message for this iid
-    if (ii->iid == INST_INFO_EMPTY) {
-        LOG(DBG, ("Received first message for instance:%u\n", aa->iid));
-        ii->iid = aa->iid;
-        ii->last_update_ballot = aa->ballot;
-    }
-    assert(ii->iid == aa->iid);
-    
-    // Instance closed already, drop
-    if (IS_CLOSED(ii)) {
-        LOG(DBG, ("Dropping accept_ack for iid:%u, already closed\n", aa->iid));
-        return 0;
-    }
-    
-    // No previous message to overwrite for this acceptor
-    if (ii->acks[aa->acceptor_id] == NULL) {
-        LOG(DBG, ("Got first ack for iid:%u, acceptor:%d\n", \
-            ii->iid, aa->acceptor_id));
-        //Save this accept_ack
-        instance_store_accept(ii, aa);
-        return 1;
-    }
-    
-    //There is already a message from the same acceptor
-    accept_ack* prev_ack = ii->acks[aa->acceptor_id];
-    
-    // Already more recent info in the record, accept_ack is old
-    if (prev_ack->ballot >= aa->ballot) {
-        LOG(DBG, ("Dropping accept_ack for iid:%u, stored ballot is newer or equal\n", aa->iid));
-        return 0;
-    }
-    
-    //Replace the previous ack since the received ballot is newer
-    LOG(DBG, ("Overwriting previous accept_ack for iid:%u\n", aa->iid));
-    PAX_FREE(prev_ack);
-    instance_store_accept(ii, aa);
-
-    return 1;
-}
-
-//Checks if a given instance is closed, that is if a quorum of acceptor
-// accepted the same value+ballot
-//Returns 1 if the instance is closed, 0 otherwise
-static int 
-instance_has_quorum(struct instance* ii)
-{
-    size_t i, a_valid_index = -1, count = 0;
-    accept_ack * curr_ack;
-    
-    //Iterates over stored acks
-    for (i = 0; i < N_OF_ACCEPTORS; i++) {
-        curr_ack = ii->acks[i];
-        
-        // skip over missing acceptor acks
-        if (curr_ack == NULL)
-            continue;
-        
-        // Count the ones "agreeing" with the last added
-        if (curr_ack->ballot == ii->last_update_ballot) {
-            count++;
-            a_valid_index = i;
-            
-            // Special case: an acceptor is telling that
-            // this value is -final-, it can be delivered immediately.
-            if (curr_ack->is_final) {
-                //For sure >= than quorum...
-                count += N_OF_ACCEPTORS;
-                break;
-            }
-        }
-    }
-    
-    //Reached a quorum/majority!
-    if (count >= QUORUM) {
-        LOG(DBG, ("Reached quorum, iid:%u is closed!\n", ii->iid));
-        ii->final_value = ii->acks[a_valid_index];
-        return 1;
-    }
-    
-    //No quorum yet...
-    return 0;
-}
-
-//Invoked when the current_iid is closed.
-// Since other instances may be closed too (curr+1, curr+2), also tries to deliver them
-static void
 learner_deliver_next_closed(struct learner* l)
 {
-    //Get next instance (last delivered + 1)
-    accept_ack * aa;
-	int proposer_id;
-    struct instance * ii = learner_get_instance(l, l->current_iid);
-
-	// deliver all subsequent instances
-    while (IS_CLOSED(ii)) {
-        assert(ii->iid == l->current_iid);
-        aa = ii->final_value;
-        
-        //Deliver the value trough callback
-		proposer_id = aa->ballot % MAX_N_OF_PROPOSERS;
-		
-        l->delfun(aa->value, aa->value_size, l->current_iid, 
-			aa->ballot, proposer_id, l->delarg);
-        
-        //Clear the state
-		learner_clear_instance(l, ii->iid);
-        
-        //Move to next instance
-        l->current_iid++;
-		ii = learner_get_instance(l, l->current_iid);
-    }
+	accept_ack* ack;
+	int prop_id;
+	while ((ack = learner_state_deliver_next(l->state)) != NULL) {
+		// deliver the value through callback
+		prop_id = ack->ballot % MAX_N_OF_PROPOSERS;
+		l->delfun(ack->value, ack->value_size, ack->iid, 
+			ack->ballot, prop_id, l->delarg);
+		free(ack);
+	}
 }
-
-// //Creates a batch of repeat_req to send to the acceptor, 
-// // asking to retransmit their accepted value for a given instance
-// static void 
-// lea_send_repeat_request(iid_t from, iid_t to) {
-//     iid_t i;
-//     
-//     l_inst_info * ii;
-//     //Create empty repeat_request in buffer
-//     sendbuf_clear(to_acceptors, repeat_reqs, -1);
-//     
-//     for(i = from; i < to; i++) {
-//         //Request all non-closed in from...to range
-//         ii = GET_LEA_INSTANCE(i);
-//         if(!IS_CLOSED(ii)) {
-//             sendbuf_add_repeat_req(to_acceptors, i);
-//         }
-//     }
-//     //Flush if dirty flag is set
-//     sendbuf_flush(to_acceptors);
-// }
-
-//This function is invoked periodically and tries to detect if the learner 
-// missed some message. For example if instance I is closed but instance I-1 
-// it's not, we can't deliver I. So it will ask to the acceptors to repeat 
-// their accepted value
-// static void
-// lea_hole_check(int fd, short event, void *arg) {
-//     //Periodic check for missing instances
-//     //(i.e. i+1 closed, but i not closed yet)
-//     if (highest_iid_seen > current_iid + LEARNER_ARRAY_SIZE) {
-//         LOG(0, ("This learner is lagging behind!!!, highest seen:%u, highest delivered:%u\n", 
-//             highest_iid_seen, current_iid-1));
-//         lea_send_repeat_request(current_iid, highest_iid_seen);
-//     } else if (highest_iid_closed > current_iid) {
-//         LOG(VRB, ("Out of sync, highest closed:%u, highest delivered:%u\n", 
-//             highest_iid_closed, current_iid-1));
-//         //Ask retransmission to acceptors
-//         lea_send_repeat_request(current_iid, highest_iid_closed);
-//     }
-// 
-//     //Set the next timeout for calling this function
-//     if (event_add(&hole_check_event, &hole_check_interval) != 0) {
-// 	   printf("Error while adding next hole_check event\n");
-// 	}
-// }
-
-/*-------------------------------------------------------------------------*/
-// Event handlers
-/*-------------------------------------------------------------------------*/
 
 // Called when an accept_ack is received, the learner will update it's status
 // for that instance and afterward check if the instance is closed
 static void
 learner_handle_accept_ack(struct learner* l, accept_ack * aa)
 {
-	int relevant;
-	struct instance* ii;
-	
-	// Keep track of highest seen instance id
-	if (aa->iid > l->highest_iid_seen) {
-		l->highest_iid_seen = aa->iid;
-	}
 
-	//Already closed and delivered, ignore message
-	if (aa->iid < l->current_iid) {
-		LOG(DBG, ("Dropping accept_ack for already delivered iid:%u\n", aa->iid));
-		return;
-	}
-
-	//We are late w.r.t the current iid, ignore message
-	// (The instance received is too ahead and will overwrite something)
-	if (aa->iid >= l->current_iid + LEARNER_ARRAY_SIZE) {
-		LOG(DBG, ("Dropping accept_ack for iid:%u, too far in future\n", aa->iid));
-		return;
-	}
-
-	//Message is within interesting bounds
-	//Update the corresponding record
-
-	ii = learner_get_instance(l, aa->iid);
-	relevant = instance_update(ii, aa);
-
-	if (!relevant) {
-		//Not really interesting (i.e. a duplicate message)
-		LOG(DBG, ("Learner discarding learn for iid:%u\n", aa->iid));
-		return;
-	}
-
-	// Message contained some relevant info, 
-	// check if instance can be declared closed
-	int closed = instance_has_quorum(ii);
-	if (!closed) {
-		LOG(DBG, ("Not yet a quorum for iid:%u\n", aa->iid));
-		return;
-	}
-
-	// Keep track of highest closed
-	if (ii->iid > l->highest_iid_closed)
-		l->highest_iid_closed = ii->iid;
-
-	//If the closed instance is the current one,
-	//Deliver it (and the followings if already closed)
-	if (aa->iid == l->current_iid) {
-		learner_deliver_next_closed(l);
-	}
+	learner_state_receive_accept(l->state, aa);
+	learner_deliver_next_closed(l);
 }
 
 static void
@@ -401,13 +128,8 @@ do_connect(struct learner* l, struct event_base* b, address* a)
 	return bev;
 }
 
-/*-------------------------------------------------------------------------*/
-// Public functions (see libpaxos.h for more details)
-/*-------------------------------------------------------------------------*/
-
 struct learner*
-learner_init_conf(struct config* c, deliver_function f, void* arg,
-struct event_base* b)
+learner_init_conf(struct config* c, deliver_function f, void* arg, struct event_base* b)
 {
 	int i;
 	struct learner* l;
@@ -415,23 +137,9 @@ struct event_base* b)
 	l = malloc(sizeof(struct learner));
 	l->conf = c;
 	l->base = b;
-	l->highest_iid_seen = 1; 
-	l->current_iid = 1;
-	l->highest_iid_closed = 0;
 	l->delfun = f;
 	l->delarg = arg;
-
-	// check array size
-	if ((LEARNER_ARRAY_SIZE & (LEARNER_ARRAY_SIZE -1)) != 0) {
-		printf("Error: LEARNER_ARRAY_SIZE is not a power of 2\n");
-		return NULL;
-	}
-	
-	// clear the state array
-	memset(l->instances, 0, (sizeof(struct instance) * LEARNER_ARRAY_SIZE));
-	for (i = 0; i < LEARNER_ARRAY_SIZE; i++) {
-		learner_clear_instance(l, i);
-	}
+	l->state = learner_state_new(LEARNER_ARRAY_SIZE);
 	
 	// setup connections to acceptors
 	for (i = 0; i < l->conf->acceptors_count; i++) {
---
+++ lib/learner_state.c
@@ -0,0 +1,280 @@
+#include "learner_state.h"
+#include "carray.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+
+struct instance
+{
+    iid_t           iid;
+    ballot_t        last_update_ballot;
+    accept_ack*     acks[N_OF_ACCEPTORS];
+    accept_ack*     final_value;
+};
+
+
+struct learner_state
+{
+	// Highest instance closed (can be higher than current!)
+	// TODO: not used
+	iid_t highest_iid_closed;
+	//Highest instance for which a message was seen
+	iid_t highest_iid_seen;
+	// Current instance, incremented when current is closed and 
+	// the corresponding value is delivered
+	iid_t current_iid;
+	// the instances we store
+	struct carray* instances;
+};
+
+
+static void
+instance_clear(struct instance* inst)
+{
+	assert(inst != NULL);
+	memset(inst, 0, sizeof(struct instance));
+}
+
+static void
+instance_deep_clear(struct instance* inst)
+{	
+	int i;
+    for (i = 0; i < N_OF_ACCEPTORS; i++)
+        if (inst->acks[i] != NULL)
+            free(inst->acks[i]);
+	instance_clear(inst);
+}
+
+static struct instance*
+instance_new()
+{
+	struct instance* inst;
+	inst = malloc(sizeof(struct instance));
+	instance_clear(inst);
+	return inst;
+}
+
+
+/*
+	Checks if a given instance is closed, that is if a quorum of acceptor
+	accepted the same value ballot pair.
+	Returns 1 if the instance is closed, 0 otherwise
+*/
+static int 
+instance_has_quorum(struct instance* inst)
+{
+    accept_ack * curr_ack;
+    int i, a_valid_index = -1, count = 0;
+
+	if (inst->final_value != NULL)
+		return 1;
+    
+    //Iterates over stored acks
+    for (i = 0; i < N_OF_ACCEPTORS; i++) {
+        curr_ack = inst->acks[i];
+        
+        // skip over missing acceptor acks
+        if (curr_ack == NULL)
+            continue;
+        
+        // Count the ones "agreeing" with the last added
+        if (curr_ack->ballot == inst->last_update_ballot) {
+            count++;
+            a_valid_index = i;
+            
+            // Special case: an acceptor is telling that
+            // this value is -final-, it can be delivered immediately.
+            if (curr_ack->is_final) {
+                //For sure >= than quorum...
+                count += N_OF_ACCEPTORS;
+                break;
+            }
+        }
+    }
+    
+    //Reached a quorum/majority!
+    if (count >= QUORUM) {
+        LOG(DBG, ("Reached quorum, iid: %u is closed!\n", inst->iid));
+        inst->final_value = inst->acks[a_valid_index];
+        return 1;
+    }
+    
+    //No quorum yet...
+    return 0;
+}
+
+/*
+	Adds the given accept_ack for the given instance.
+	Assumes inst->acks[acceptor_id] was already freed.
+*/
+static void
+instance_add_accept(struct instance* inst, accept_ack* ack)
+{
+	accept_ack * new_ack;
+	new_ack = malloc(ACCEPT_ACK_SIZE(ack));
+	memcpy(new_ack, ack, ACCEPT_ACK_SIZE(ack));
+    inst->acks[ack->acceptor_id] = new_ack;
+    inst->last_update_ballot = ack->ballot;
+}
+
+/*
+	Tries to update the state based on the accept_ack received.
+ 	Returns 0 if the message was discarded because not relevant,
+	otherwise 1.
+*/
+static int
+instance_update(struct instance* inst, accept_ack* ack)
+{
+	accept_ack* prev_ack;
+	
+	// First message for this iid
+    if (inst->iid == 0) {
+        LOG(DBG, ("Received first message for instance: %u\n", ack->iid));
+        inst->iid = ack->iid;
+        inst->last_update_ballot = ack->ballot;
+    }
+    assert(inst->iid == ack->iid);
+    
+    // Instance closed already, drop
+    if (instance_has_quorum(inst)) {
+		LOG(DBG, ("Dropping accept_ack for iid: %u", ack->iid));
+		LOG(DBG, ("already closed\n"));
+        return 0;
+    }
+    
+    // No previous message to overwrite for this acceptor
+    if (inst->acks[ack->acceptor_id] == NULL) {
+        LOG(DBG, ("Got first ack for: %u, acceptor: %d\n", 
+			inst->iid, ack->acceptor_id));
+        //Save this accept_ack
+        instance_add_accept(inst, ack);
+        return 1;
+    }
+    
+    // There is already a message from the same acceptor
+	prev_ack = inst->acks[ack->acceptor_id];
+    
+    // Already more recent info in the record, accept_ack is old
+    if (prev_ack->ballot >= ack->ballot) {
+		LOG(DBG, ("Dropping accept_ack for iid: %u ", ack->iid));
+		LOG(DBG, ("stored ballot is newer or equal\n"));
+        return 0;
+    }
+    
+    // Replace the previous ack since the received ballot is newer
+    LOG(DBG, ("Overwriting previous accept_ack for iid: %u\n", ack->iid));
+    free(prev_ack);
+    instance_add_accept(inst, ack);
+
+    return 1;
+}
+
+static struct instance*
+learner_state_get_instance(struct learner_state* s, iid_t iid)
+{
+	struct instance* inst;
+	inst = carray_at(s->instances, iid);
+	assert(inst->iid == iid || inst->iid == 0);
+	return inst;
+}
+
+
+static struct instance*
+learner_state_get_current_instance(struct learner_state* s)
+{
+	return learner_state_get_instance(s, s->current_iid);
+}
+
+
+accept_ack*
+learner_state_deliver_next(struct learner_state* s)
+{
+	struct instance* inst;
+	accept_ack* ack = NULL;
+	inst = learner_state_get_current_instance(s);
+ 	if (instance_has_quorum(inst)) {
+		size_t size = ACCEPT_ACK_SIZE(inst->final_value);
+		
+		// make a copy of the accept_ack to deliver,
+		// before clearing the instance
+		ack = malloc(size);
+		memcpy(ack, inst->final_value, size);
+
+		instance_deep_clear(inst);
+		s->current_iid++;
+	}
+	return ack;
+}
+
+void
+learner_state_receive_accept(struct learner_state* s, accept_ack* ack)
+{
+	int relevant;
+	struct instance* inst;
+	
+	// Keep track of highest seen instance id
+	if (ack->iid > s->highest_iid_seen) {
+		s->highest_iid_seen = ack->iid;
+	}
+
+	// Already closed and delivered, ignore message
+	if (ack->iid < s->current_iid) {
+		LOG(DBG, ("Dropping accept_ack for already delivered iid: %u\n",
+			ack->iid));
+		return;
+	}
+
+	// We are late w.r.t the current iid, ignore message
+	// (The instance received is too ahead and will overwrite something)
+	if (ack->iid >= s->current_iid + carray_size(s->instances)) {
+		LOG(DBG, ("Dropping accept_ack for iid: %u, too far in future\n",
+			ack->iid));
+		return;
+	}
+
+	// Message is within interesting bounds
+	// Update the corresponding record
+
+	inst = learner_state_get_instance(s, ack->iid);
+	relevant = instance_update(inst, ack);
+
+	if (!relevant) {
+		//Not really interesting (i.e. a duplicate message)
+		LOG(DBG, ("Learner discarding learn for iid: %u\n", ack->iid));
+		return;
+	}
+
+	// Message contained some relevant info, 
+	// check if instance can be declared closed
+	if (!instance_has_quorum(inst)) {
+		LOG(DBG, ("Not yet a quorum for iid: %u\n", ack->iid));
+		return;
+	}
+
+	// Keep track of highest closed
+	if (inst->iid > s->highest_iid_closed)
+		s->highest_iid_closed = inst->iid;
+}
+
+static void
+initialize_instances(struct learner_state* s, int count)
+{
+	int i;
+	s->instances = carray_new(count);
+	assert(s->instances != NULL);	
+	for (i = 0; i < carray_size(s->instances); i++)
+		carray_push_back(s->instances, instance_new());
+}
+
+struct learner_state*
+learner_state_new(int instances)
+{
+	struct learner_state* s;
+	s = malloc(sizeof(struct learner_state));
+	initialize_instances(s, instances);
+	s->highest_iid_seen = 1;
+	s->highest_iid_closed = 0;
+	s->current_iid = 1;
+	return s;
+}
