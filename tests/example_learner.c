95c7fee5223c8ecdfae361f1a47d960c5d80accf
sciascid <sciascid@4b45d5e3-6afd-4c2a-bf5d-36b89e0d2062>
2013-01-22 09:46:07 +0000

    Fixes codes style of example learner.
    git-svn-id: svn+ssh://atelier.inf.usi.ch/home/sciascid/svnroot/libpaxos@615 4b45d5e3-6afd-4c2a-bf5d-36b89e0d2062

--- tests/example_learner.c
+++ tests/example_learner.c
@@ -5,8 +5,8 @@
 
 #include "libpaxos.h"
 
-void
-handle_cltr_c (int sig)
+static void
+handle_cltr_c(int sig)
 {
 	printf("Caught signal %d\n", sig);
     exit(0);
@@ -15,11 +15,9 @@ handle_cltr_c (int sig)
 static char
 as_char(char c)
 {
-    if(c < 33 || c > 126) {
-        return '!';        
-    } else {
-        return c;
-    }
+    if (c < 33 || c > 126)
+		return '!';
+	return c;
 }
 
 static void
@@ -28,11 +26,14 @@ deliver(char* value, size_t size, iid_t iid, ballot_t b,
 {
     printf("Paxos instance %u closed by ballot %u\n", iid, b);
     printf("Value (by proposer:%d, size: %d) ->", proposer, (int)size);
-    printf("[%c][%c][%c][...]\n", as_char(value[0]), as_char(value[1]), as_char(value[2]));
+    printf("[%c][%c][%c][...]\n", 
+		as_char(value[0]), 
+		as_char(value[1]), 
+		as_char(value[2]));
 }
 
 int
-main (int argc, char const *argv[])
+main(int argc, char const *argv[])
 {
 	struct learner* l;
 	struct event_base* b;
