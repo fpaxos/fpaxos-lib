1059a1cb4631bb4b421efacf643681986c310d31
sciascid <sciascid@4b45d5e3-6afd-4c2a-bf5d-36b89e0d2062>
2013-03-13 09:33:49 +0000

    Adds CMake build systems.
    git-svn-id: svn+ssh://atelier.inf.usi.ch/home/sciascid/svnroot/libpaxos@667 4b45d5e3-6afd-4c2a-bf5d-36b89e0d2062

---
+++ cmake/FindBDB.cmake
@@ -0,0 +1,23 @@
+# - Find BDB
+# Find the native BDB includes and library
+#
+# BDB_INCLUDES - where to find db.h
+# BDB_LIBRARIES - List of libraries when using BDB.
+# BDB_FOUND - True if BDB found.
+
+set(BDB_ROOT "" CACHE STRING "BerkeleyDB root directory")
+
+find_path(BDB_INCLUDE_DIR db.h HINTS "${BDB_ROOT}/include"  )
+find_library(BDB_LIBRARY db  HINTS "${BDB_ROOT}/lib")
+
+set(BDB_LIBRARIES ${BDB_LIBRARY})
+set(BDB_INCLUDE_DIRS ${BDB_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+
+# handle the QUIETLY and REQUIRED arguments and set BDB_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(BDB DEFAULT_MSG
+                                  BDB_LIBRARY BDB_INCLUDE_DIR)
+
+mark_as_advanced(BDB_INCLUDE_DIR BDB_LIBRARY)
---
+++ cmake/FindLIBEVENT.cmake
@@ -0,0 +1,27 @@
+# - Find LIBEVENT
+# Find the native LibEvent includes and library
+#
+# LIBEVENT_INCLUDES - where to find event.h
+# LIBEVENT_LIBRARIES - List of libraries when using LIBEVENT.
+# LIBEVENT_FOUND - True if LibEvent found.
+
+set(LIBEVENT_ROOT "" CACHE STRING "LIBEVENT root directory")
+
+find_path(LIBEVENT_INCLUDE_DIR event.h 
+    HINTS "${LIBEVENT_ROOT}/include")
+
+find_library(LIBEVENT_LIBRARY
+   NAMES libevent.a event
+   HINTS "${LIBEVENT_ROOT}/lib")
+
+set(LIBEVENT_LIBRARIES ${LIBEVENT_LIBRARY})
+set(LIBEVENT_INCLUDE_DIRS ${LIBEVENT_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+
+# handle the QUIETLY and REQUIRED arguments and set LIBEVENT_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(LIBEVENT DEFAULT_MSG
+                                  LIBEVENT_LIBRARY LIBEVENT_INCLUDE_DIR)
+
+mark_as_advanced(LIBEVENT_INCLUDE_DIR LIBEVENT_LIBRARY)
