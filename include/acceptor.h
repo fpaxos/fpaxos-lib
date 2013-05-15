0d935cc80318e37d32ed507e24be4121abc6ef69
Daniele Sciascia <daniele.sciascia@gmail.com>
2013-05-15 23:26:53 +0200

    Learner recovery complete.

--- include/acceptor.h
+++ include/acceptor.h
@@ -14,6 +14,7 @@ struct acceptor* acceptor_new(int id);
 int acceptor_delete(struct acceptor* s);
 acceptor_record* acceptor_receive_prepare(struct acceptor* s, prepare_req* req);
 acceptor_record* acceptor_receive_accept(struct acceptor* s, accept_req* req);
+acceptor_record* acceptor_receive_repeat(struct acceptor* a, iid_t iid);
 
 #ifdef __cplusplus
 }
--- unit/learner_unittest.cc
+++ unit/learner_unittest.cc
@@ -4,12 +4,12 @@
 class LearnerTest : public testing::Test {
 protected:
 
-	int instances;
 	struct learner* l;
 	        
 	virtual void SetUp() {
-		instances = 100;
-		l = learner_new(instances);
+		int instances = 100;
+		int recover = 1;
+		l = learner_new(instances, recover);
 	}
 	
 	virtual void TearDown() { }
@@ -124,4 +124,62 @@ TEST_F(LearnerTest, IgnoreOlderBallot) {
 	delivered = learner_deliver_next(l);
 	ASSERT_EQ(delivered->iid, 1);
 	ASSERT_EQ(delivered->ballot, 201);
+}
+
+TEST_F(LearnerTest, NoHoles) {
+	accept_ack a;
+	accept_ack* delivered;
+	iid_t from, to;
+	
+	a =	(accept_ack) {1, 1, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	a =	(accept_ack) {2, 1, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	learner_deliver_next(l);
+	
+	ASSERT_EQ(learner_has_holes(l, &from, &to), 0);
+}
+
+TEST_F(LearnerTest, OneHole) {
+	accept_ack a;
+	accept_ack* delivered;
+	iid_t from, to;
+	
+	a =	(accept_ack) {1, 1, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	a =	(accept_ack) {2, 1, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	learner_deliver_next(l);
+	
+	a =	(accept_ack) {1, 3, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	a =	(accept_ack) {2, 3, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	learner_deliver_next(l);
+	
+	ASSERT_EQ(1, learner_has_holes(l, &from, &to));
+	ASSERT_EQ(2, from);
+	ASSERT_EQ(3, to);
+}
+
+TEST_F(LearnerTest, ManyHoles) {
+	accept_ack a;
+	accept_ack* delivered;
+	iid_t from, to;
+	
+	a =	(accept_ack) {1, 2, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	a =	(accept_ack) {2, 2, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	learner_deliver_next(l);
+	
+	a =	(accept_ack) {1, 100, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	a =	(accept_ack) {2, 100, 101, 101, 0, 0};
+	learner_receive_accept(l, &a);
+	learner_deliver_next(l);
+	
+	ASSERT_EQ(1, learner_has_holes(l, &from, &to));
+	ASSERT_EQ(1, from);
+	ASSERT_EQ(100, to);
 }
