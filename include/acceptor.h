0a227a9d8efd8decb4bfad3789c667c3f12af9b4
Daniele Sciascia <daniele.sciascia@gmail.com>
2013-06-06 23:48:21 +0200

    Adds COPYING and license headers.

--- include/acceptor.h
+++ include/acceptor.h
@@ -1,3 +1,23 @@
+/*
+	Copyright (C) 2013 University of Lugano
+
+	This file is part of LibPaxos.
+
+	LibPaxos is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Libpaxos is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with LibPaxos.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef _ACCEPTOR_H_
 #define _ACCEPTOR_H_
 
