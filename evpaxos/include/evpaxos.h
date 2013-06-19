61685910cc2967064f14a77f135a19523c4e2fe6
Daniele Sciascia <daniele.sciascia@gmail.com>
2013-06-19 09:00:50 +0200

    Fixes the comments in evpaxos.h.
    Previous comments were wrong leftover from libpaxos2.

--- evpaxos/include/evpaxos.h
+++ evpaxos/include/evpaxos.h
@@ -43,68 +43,42 @@ struct config
 	address acceptors[MAX_ADDR];
 };
 
-/* 
-	When starting a learner you must pass a function to be invoked whenever
-	a value is delivered.
-	This defines the type of such function.
-	Example: 
-	void my_deliver_fun(char * value, size_t size, iid_t iid, ballot_t ballot, int proposer) {
-		...
-	}
-*/
-typedef void (* deliver_function)(char*, size_t, iid_t, ballot_t, int, void*);
+/* When starting a learner you must pass a callback to be invoked whenever
+ * a value has been learned.*/
+typedef void (*deliver_function)(char*, size_t, iid_t, ballot_t, int, void*);
 
-/*
-    Starts a learner and returns when the initialization is complete.
-    Return value is 0 if successful
-    f -> A deliver_function invoked when a value is delivered.
-         This argument cannot be NULL
-         It's called by an internal thread therefore:
-         i)  it must be quick
-         ii) you must synchronize/lock externally if this function touches data
-             shared with some other thread (i.e. the one that calls learner init)
-    cif -> A custom_init_function invoked by the internal libevent thread, 
-           invoked when the normal learner initialization is completed
-           Can be used to add other events to the existing event loop.
-           It's ok to pass NULL if you don't need it.
-           cif has to return -1 for error and 0 for success
-*/
+/* Initializes a learner with a given config file, a deliver callback,
+ * an optional argument to that is passed to the callback, and
+ * a libevent event_base. */
 struct evlearner*
 evlearner_init(const char* config_file, deliver_function f, void* arg,
 	struct event_base* base);
 
-void evlearner_free(struct evlearner* l);
+/* Release the memory allocated by the learner */
+void
+evlearner_free(struct evlearner* l);
 
-/*
-	Starts an acceptor and returns when the initialization is complete.
-	Return value is 0 if successful
-	acceptor_id -> Must be in the range [0...(N_OF_ACCEPTORS-1)]
-*/
+/* Initializes a acceptor with a given id (which MUST be unique),
+ * a config file and a libevent event_base.
+ * id -> Must be in the range [0...(MAX_N_OF_ACCEPTORS-1)] */
 struct evacceptor*
 evacceptor_init(int id, const char* config, struct event_base* b);
 
-/*
-	Shuts down the acceptor in the current process.
-	It may take a few seconds to complete since the DB needs to be closed.
-*/
+/* Frees the memory allocated by the acceptor. This will also cleanly close the  * underlying storage. */
 int
 evacceptor_free(struct evacceptor* a);
 
-/*
-	Starts a proposer with the given ID (which MUST be unique).
-	Return value is 0 if successful
-	proposer_id -> Must be in the range [0...(MAX_N_OF_PROPOSERS-1)]
-*/
+/* Initializes a proposer with a given ID (which MUST be unique), 
+ * a config file and a libevent event_base.
+ * id -> Must be in the range [0...(MAX_N_OF_PROPOSERS-1)] */
 struct evproposer*
 evproposer_init(int id, const char* config, struct event_base* b);
 
+/* Release the memory allocated by the proposer */
+void
+evproposer_free(struct evproposer* p);
 
-void evproposer_free(struct evproposer* p);
-
-
-/*
-	Function used for submitting values to a proposer.
-*/
+/* Used by clients to submit values to proposers. */
 void
 paxos_submit(struct bufferevent* bev, char* value, int size);
 
